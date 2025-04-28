e2e2ca6adc6453281b232062548ebbe8f312e7ad
Abdullah Feroz <00maferoz@gmail.com>
2025-04-28 21:38:41 +0500

    Integrated model prediction

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import joblib
+import numpy as np
 
 model = joblib.load("SVC_model.pkl")
 
@@ -13,11 +14,17 @@ def home():
 
 @app.route("/predict", methods = ['POST'])
 def predict():
-    data = request.get_json(force=True)
-    # Here you would typically load your model and make a prediction
-    # For demonstration, we'll just return the received data
-    prediction = data
-    return jsonify(prediction)
-
+    try:
+        data = request.get_json()
+        feature_list = data["pixels"]
+        
+        feature_array = np.array(feature_list).reshape(1, -1)
+        # print(f"Feature list shape: {feature_array.shape}")
+        # print(f"Feature list: {feature_array}")
+        prediction = model.predict(feature_array)
+        return jsonify(prediction.tolist())
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({"error": str(e)}), 400
 if __name__ == '__main__':
     app.run(debug=True)
