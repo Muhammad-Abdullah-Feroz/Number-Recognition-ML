d7d5ea35b712255c11d6589f7a8eeab8b4c95cdf
Abdullah Feroz <00maferoz@gmail.com>
2025-05-04 13:57:17 +0500

    Added multiple models

--- server.py
+++ server.py
@@ -3,7 +3,10 @@ from flask_cors import CORS
 import joblib
 import numpy as np
 
-model = joblib.load("SVC_model.pkl")
+SVC = joblib.load("./Models/SVC_model.pkl")
+KNN = joblib.load("./Models/KNN_model.pkl")
+LR = joblib.load("./Models/LR_model.pkl")
+NB = joblib.load("./Models/NaiveBayes_model.pkl")
 
 app = Flask(__name__)
 CORS(app)
@@ -12,8 +15,8 @@ CORS(app)
 def home():
     return render_template('./index.html')
 
-@app.route("/predict", methods = ['POST'])
-def predict():
+@app.route("/predict-svc", methods = ['POST'])
+def predictSVC():
     try:
         data = request.get_json()
         feature_list = data["pixels"]
@@ -21,10 +24,60 @@ def predict():
         feature_array = np.array(feature_list).reshape(1, -1)
         # print(f"Feature list shape: {feature_array.shape}")
         # print(f"Feature list: {feature_array}")
-        prediction = model.predict(feature_array)
+        prediction = SVC.predict(feature_array)
         return jsonify(prediction.tolist())
     except Exception as e:
         print(f"Error: {e}")
         return jsonify({"error": str(e)}), 400
+    
+
+@app.route("/predict-lr", methods = ['POST'])
+def predictLR():
+    try:
+        data = request.get_json()
+        feature_list = data["pixels"]
+        
+        feature_array = np.array(feature_list).reshape(1, -1)
+        # print(f"Feature list shape: {feature_array.shape}")
+        # print(f"Feature list: {feature_array}")
+        prediction = LR.predict(feature_array)
+        return jsonify(prediction.tolist())
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({"error": str(e)}), 400
+    
+
+@app.route("/predict-knn", methods = ['POST'])
+def predictKNN():
+    try:
+        data = request.get_json()
+        feature_list = data["pixels"]
+        
+        feature_array = np.array(feature_list).reshape(1, -1)
+        # print(f"Feature list shape: {feature_array.shape}")
+        # print(f"Feature list: {feature_array}")
+        prediction = KNN.predict(feature_array)
+        return jsonify(prediction.tolist())
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({"error": str(e)}), 400
+    
+    
+@app.route("/predict-nb", methods = ['POST'])
+def predictNB():
+    try:
+        data = request.get_json()
+        feature_list = data["pixels"]
+        
+        feature_array = np.array(feature_list).reshape(1, -1)
+        # print(f"Feature list shape: {feature_array.shape}")
+        # print(f"Feature list: {feature_array}")
+        prediction = NB.predict(feature_array)
+        return jsonify(prediction.tolist())
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({"error": str(e)}), 400
+    
+    
 if __name__ == '__main__':
     app.run(debug=True)
